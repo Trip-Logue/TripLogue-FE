0d886dd7aaa8def560a9c659532f9bef16b76e29
enohs <kmsn01@naver.com>
2025-07-14 17:38:04 +0900

    FEAT: 로그인 페이지 제작

--- src/assets/pages/mainPage.tsx
+++ src/assets/pages/mainPage.tsx
@@ -1,13 +1,31 @@
-import { MainSidebar } from "./mainSidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { MainSidebar } from './mainSidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import CommonBtn from '@/components/commons/commonBtn';
+import { useRoute } from '@/hooks/useRoute';
 
 function MainPage() {
+  const { routeToLogin, routeToSignup } = useRoute();
+
   return (
-    <div className="flex flex-col gap-4 bg-white-100">
-      <SidebarProvider >
-        <MainSidebar />
-      </SidebarProvider>
-    </div>
+    <>
+      <div className='flex'>
+        <div className='flex flex-col gap-4 bg-white-100'>
+          <SidebarProvider>
+            <MainSidebar />
+          </SidebarProvider>
+        </div>
+        <div className='flex-1 flex justify-end p-4 gap-2'>
+          <CommonBtn
+            onClick={routeToLogin}
+            text='로그인'
+            className='bg-[#D9D9D9] text-[#1A1A1A] border border-transparent rounded-lg px-4 py-2 font-medium text-base hover:bg-[#C0C0C0] transition-colors duration-200 w-24 h-10'></CommonBtn>
+          <CommonBtn
+            onClick={routeToSignup}
+            text='회원가입'
+            className='bg-[#D9D9D9] text-[#1A1A1A] border border-transparent rounded-lg px-4 py-2 font-medium text-base hover:bg-[#C0C0C0] transition-colors duration-200 w-28 h-10'></CommonBtn>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export interface CommonBtnProps {
   text: string;
+  type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
   className?: string;
 }
@@ -9,4 +10,14 @@ export interface CommonInputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
+}
+
+export interface RegisterInputProps {
+  id?: string;
+  type?: 'text' | 'password' | 'email';
+  className?: string;
+  value: string;
+  placeholder?: string;
+  autoFocus?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
